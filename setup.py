3702a41880aa77fc121ce4b274a6093564d4b4d4
Ryan Jacobs <ryan@rmj.us>
2019-08-13 20:53:32 -0700

    Revert "remove OSI from LICENSE"
    This reverts commit 9e400ee81af2eb71aa2e4d2c3bd681e31c3fc99c.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
