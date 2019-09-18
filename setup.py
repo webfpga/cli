4e3455247de41a8e66199c41ccf3ff880691f805
Ryan Jacobs <ryan@rmj.us>
2019-09-18 03:12:53 -0700

    setup reqs

--- setup.py
+++ setup.py
@@ -17,6 +17,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/webfpga/cli",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "pyusb>=1.0.2",
+        "click>=7.0",
+        "requests>=2.22.0",
+        "websockets>=8.0.2",
+        "colorama>=0.4.1",
+        "termcolor>=1.1.0"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
