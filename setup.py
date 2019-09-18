a123843f7093745b0563cd46ac682f5779d42ae2
Ryan Jacobs <ryan@rmj.us>
2019-09-17 19:33:49 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='webfpga',  
+    version='0.1',
+    scripts=['webfpga'] ,
+    author="WebFPGA",
+    author_email="support@webfpga.io",
+    description="Official WebFPGA Command-line Utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/webfpga/cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
